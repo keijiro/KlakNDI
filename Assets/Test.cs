0330408b2f913b90583ea8d88561ed5adb754776
Keijiro Takahashi <keijiro@gmail.com>
2018-04-05 22:13:40 +0900

    Observer implementation.

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,85 +1,38 @@
-﻿using System.Runtime.InteropServices;
-using System.Threading;
+using System.Runtime.InteropServices;
 using System;
 using UnityEngine;
 using Klak.Ndi;
 
 public class Test : MonoBehaviour
 {
-    #region Receiver thread
-
     IntPtr _receiver;
-    AutoResetEvent _receiverSync;
-    bool _receiverStop;
-    bool _frameReady;
-
-    void ReceiverThreadEntry()
-    {
-        while (_receiver == IntPtr.Zero)
-        {
-            if (_receiverStop) return;
-            _receiver = PluginEntry.NDI_CreateReceiver();
-        }
-
-        Debug.Log("start loop");
-
-        while (true)
-        {
-            while (!_frameReady)
-            {
-                if (_receiverStop) return;
-                _frameReady = PluginEntry.NDI_ReceiveFrame(_receiver);
-            }
-
-        Debug.Log("wait");
-
-            _receiverSync.WaitOne();
-
-            PluginEntry.NDI_FreeFrame(_receiver);
-            _frameReady = false;
-        }
-    }
-
-    #endregion
-
-    Thread _receiverThread;
     Texture2D _texture;
 
     void Start()
     {
         PluginEntry.NDI_Initialize();
-
-        _receiverSync = new AutoResetEvent(false);
-
-        _receiverThread = new Thread(ReceiverThreadEntry);
-        _receiverThread.Start();
     }
 
     void OnDestroy()
     {
-        if (_receiverThread != null)
-        {
-            _receiverStop = true;
-            _receiverSync.Set();
-            _receiverThread.Join();
-        }
-
         if (_receiver != System.IntPtr.Zero)
-        {
-            if (_frameReady)
-                PluginEntry.NDI_FreeFrame(_receiver);
-
             PluginEntry.NDI_DestroyReceiver(_receiver);
-        }
 
         if (_texture != null) Destroy(_texture);
+
+        PluginEntry.NDI_Finalize();
     }
 
     void Update()
     {
-        if (!_frameReady) return;
+        if (_receiver == System.IntPtr.Zero)
+        {
+            _receiver = PluginEntry.NDI_CreateReceiver();
+            if (_receiver == System.IntPtr.Zero) return;
+        }
 
-        Debug.Log("frame ready");
+        var ready = PluginEntry.NDI_ReceiveFrame(_receiver);
+        if (!ready) return;
 
         var width = PluginEntry.NDI_GetFrameWidth(_receiver);
         var height = PluginEntry.NDI_GetFrameHeight(_receiver);
@@ -92,41 +45,41 @@ public class Test : MonoBehaviour
 
         GetComponent<Renderer>().material.mainTexture = _texture;
 
-        _receiverSync.Set();
+        PluginEntry.NDI_FreeFrame(_receiver);
     }
 
     /*
-    System.IntPtr _sender;
+       System.IntPtr _sender;
 
-    void Start()
-    {
-        PluginEntry.NDI_Initialize();
-        _sender = PluginEntry.NDI_CreateSender("NDI Test");
-    }
+       void Start()
+       {
+       PluginEntry.NDI_Initialize();
+       _sender = PluginEntry.NDI_CreateSender("NDI Test");
+       }
 
-    void OnDestroy()
-    {
-        PluginEntry.NDI_DestroySender(_sender);
-    }
+       void OnDestroy()
+       {
+       PluginEntry.NDI_DestroySender(_sender);
+       }
 
-    void OnRenderImage(RenderTexture source, RenderTexture destination)
-    {
-        var tempRT = RenderTexture.GetTemporary(source.width, source.height);
-        Graphics.Blit(source, tempRT);
+       void OnRenderImage(RenderTexture source, RenderTexture destination)
+       {
+       var tempRT = RenderTexture.GetTemporary(source.width, source.height);
+       Graphics.Blit(source, tempRT);
 
-        var tempTex = new Texture2D(source.width, source.height, TextureFormat.RGBA32, false);
-        tempTex.ReadPixels(new Rect(0, 0, source.width, source.height), 0, 0, false);
-        tempTex.Apply();
+       var tempTex = new Texture2D(source.width, source.height, TextureFormat.RGBA32, false);
+       tempTex.ReadPixels(new Rect(0, 0, source.width, source.height), 0, 0, false);
+       tempTex.Apply();
 
-        var data = tempTex.GetRawTextureData();
-        var pin = GCHandle.Alloc(data, GCHandleType.Pinned);
-        PluginEntry.NDI_SendFrame(_sender, pin.AddrOfPinnedObject(), source.width, source.height);
-        pin.Free();
+       var data = tempTex.GetRawTextureData();
+       var pin = GCHandle.Alloc(data, GCHandleType.Pinned);
+       PluginEntry.NDI_SendFrame(_sender, pin.AddrOfPinnedObject(), source.width, source.height);
+       pin.Free();
 
-        Destroy(tempTex);
-        RenderTexture.ReleaseTemporary(tempRT);
+       Destroy(tempTex);
+       RenderTexture.ReleaseTemporary(tempRT);
 
-        Graphics.Blit(source, destination);
-    }
-    */
+       Graphics.Blit(source, destination);
+       }
+     */
 }
