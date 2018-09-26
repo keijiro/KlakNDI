806994a48db9aaec15185205e919e9e925bc14db
Keijiro Takahashi <keijiro@gmail.com>
2018-09-26 18:37:10 +0900

    Add iOS support

--- Assets/Klak/NDI/Plugin/PluginEntry.cs
+++ Assets/Klak/NDI/Plugin/PluginEntry.cs
@@ -1,3 +1,7 @@
+#if !UNITY_EDITOR && UNITY_IOS
+#define NDI_SENDER_ONLY
+#endif
+
 using System;
 using System.Runtime.InteropServices;
 
@@ -11,51 +15,79 @@ namespace Klak.Ndi
 
     public static class PluginEntry
     {
+        #if !UNITY_EDITOR && UNITY_IOS
+        const string _dllName = "__Internal";
+        #else
+        const string _dllName = "KlakNDI";
+        #endif
+
         #region Common functions
 
-        [DllImport("KlakNDI")]
+        #if !NDI_SENDER_ONLY
+
+        [DllImport(_dllName)]
         public static extern IntPtr NDI_GetTextureUpdateCallback();
 
-        [DllImport("KlakNDI")]
+        [DllImport(_dllName)]
         public static extern int NDI_RetrieveSourceNames(IntPtr[] destination, int maxCount);
+
+        #else
+
+        public static IntPtr NDI_GetTextureUpdateCallback() { return IntPtr.Zero; }
+        public static int NDI_RetrieveSourceNames(IntPtr[] destination, int maxCount) { return 0; }
+
+        #endif
 
         #endregion
 
         #region Sender functions
 
-        [DllImport("KlakNDI")]
+        [DllImport(_dllName)]
         public static extern IntPtr NDI_CreateSender(string name);
 
-        [DllImport("KlakNDI")]
+        [DllImport(_dllName)]
         public static extern void NDI_DestroySender(IntPtr sender);
 
-        [DllImport("KlakNDI")]
+        [DllImport(_dllName)]
         public static extern void NDI_SendFrame(IntPtr sender, IntPtr data, int width, int height, FourCC fourCC);
 
-        [DllImport("KlakNDI")]
+        [DllImport(_dllName)]
         public static extern void NDI_SyncSender(IntPtr sender);
 
         #endregion
 
         #region Receiver functions
 
-        [DllImport("KlakNDI")]
+        #if !NDI_SENDER_ONLY
+
+        [DllImport(_dllName)]
         public static extern IntPtr NDI_TryOpenSourceNamedLike(string clause);
 
-        [DllImport("KlakNDI")]
+        [DllImport(_dllName)]
         public static extern void NDI_DestroyReceiver(IntPtr receiver);
 
-        [DllImport("KlakNDI")]
+        [DllImport(_dllName)]
         public static extern uint NDI_GetReceiverID(IntPtr receiver);
 
-        [DllImport("KlakNDI")]
+        [DllImport(_dllName)]
         public static extern int NDI_GetFrameWidth(IntPtr receiver);
 
-        [DllImport("KlakNDI")]
+        [DllImport(_dllName)]
         public static extern int NDI_GetFrameHeight(IntPtr receiver);
 
-        [DllImport("KlakNDI")]
+        [DllImport(_dllName)]
         public static extern FourCC NDI_GetFrameFourCC(IntPtr receiver);
+
+        #else
+
+        public static IntPtr NDI_TryOpenSourceNamedLike(string clause) { return IntPtr.Zero; }
+        public static void NDI_DestroyReceiver(IntPtr receiver) {}
+        public static uint NDI_GetReceiverID(IntPtr receiver) { return 0; }
+        public static int NDI_GetFrameWidth(IntPtr receiver) { return 0; }
+        public static int NDI_GetFrameHeight(IntPtr receiver) { return 0; }
+        public static FourCC NDI_GetFrameFourCC(IntPtr receiver) { return 0; }
+
+        #endif
 
         #endregion
     }
