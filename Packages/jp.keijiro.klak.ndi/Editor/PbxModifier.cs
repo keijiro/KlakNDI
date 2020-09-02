2c24ad96a8558687648ea7fe2ca00512566f3245
Keijiro Takahashi <keijiro@gmail.com>
2020-09-03 00:38:52 +0900

    iOS 14 support
    - Add Bonjour service list declaration to plist.info.

--- Packages/jp.keijiro.klak.ndi/Editor/PbxModifier.cs
+++ Packages/jp.keijiro.klak.ndi/Editor/PbxModifier.cs
@@ -15,19 +15,54 @@ public class PbxModifier
     {
         if (buildTarget == BuildTarget.iOS)
         {
-            string projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
-
-            PBXProject proj = new PBXProject();
-            proj.ReadFromString(File.ReadAllText(projPath));
-
-            string target = proj.GetUnityFrameworkTargetGuid();
-            proj.AddBuildProperty(target, "LIBRARY_SEARCH_PATHS",
-                                  "/Library/NDI\\ SDK\\ for\\ Apple/lib/iOS");
-            proj.AddFrameworkToProject(target, "VideoToolbox.framework", false);
-            proj.AddFrameworkToProject(target, "libndi_ios.a", false);
-
-            File.WriteAllText(projPath, proj.WriteToString());
+            ModifyProjectFile(path);
+            ModifyPlistFile(path);
         }
+    }
+
+    static void ModifyProjectFile(string basePath)
+    {
+        var path = PBXProject.GetPBXProjectPath(basePath);
+
+        var proj = new PBXProject();
+        proj.ReadFromFile(path);
+
+        var target = proj.GetUnityFrameworkTargetGuid();
+        var libPath = "/Library/NDI\\ SDK\\ for\\ Apple/lib/iOS";
+        proj.AddBuildProperty(target, "LIBRARY_SEARCH_PATHS", libPath);
+        proj.AddFrameworkToProject(target, "VideoToolbox.framework", false);
+        proj.AddFrameworkToProject(target, "libndi_ios.a", false);
+
+        proj.WriteToFile(path);
+    }
+
+    static void ModifyPlistFile(string basePath)
+    {
+        var path = Path.Combine(basePath, "Info.plist");
+
+        var plist = new PlistDocument();
+        plist.ReadFromFile(path);
+
+        var root = plist.root;
+
+        // Bonjour service list
+        {
+            var key = "NSBonjourServices";
+            if (root.values.ContainsKey(key))
+                root.values[key].AsArray().AddString("_ndi._tcp");
+            else
+                root.CreateArray(key).AddString("_ndi._tcp");
+        }
+
+        // LAN usage description
+        {
+            var key = "NSLocalNetworkUsageDescription";
+            var desc = "NDI requires device discovery capability " +
+                       "on the networks you use.";
+            if (!root.values.ContainsKey(key)) root.SetString(key, desc);
+        }
+
+        plist.WriteToFile(path);
     }
 }
 
