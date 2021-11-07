5502bb2bab7054b3ffd3c2725a5b650dd8ac331c
Keijiro Takahashi <keijiro@gmail.com>
2021-11-07 18:02:53 +0900

    Fix: Changing target platform makes "DLL not found" error

--- jp.keijiro.klak.ndi/Runtime/Interop/Config.cs
+++ jp.keijiro.klak.ndi/Runtime/Interop/Config.cs
@@ -2,9 +2,9 @@ namespace Klak.Ndi.Interop {
 
 static class Config
 {
-    #if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+    #if UNITY_EDITOR_WIN || (!UNITY_EDITOR && UNITY_STANDALONE_WIN)
     public const string DllName = "Processing.NDI.Lib.x64";
-    #elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+    #elif UNITY_EDITOR_OSX || (!UNITY_EDITOR && UNITY_STANDALONE_OSX)
     public const string DllName = "libndi";
     #elif UNITY_EDITOR_LINUX || UNITY_STANDALONE_LINUX || UNITY_ANDROID
     public const string DllName = "ndi";
