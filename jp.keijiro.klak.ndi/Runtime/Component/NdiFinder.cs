11858939b61bf71e0e42178c4ff93d39a465c38a
JulesDoe <JulesDoe@users.noreply.github.com>
2021-11-10 12:18:45 +0100

    added possibility to get the raw NDI sources.
    to enables scripting for custom NDI source selection.

--- jp.keijiro.klak.ndi/Runtime/Component/NdiFinder.cs
+++ jp.keijiro.klak.ndi/Runtime/Component/NdiFinder.cs
@@ -5,12 +5,21 @@ namespace Klak.Ndi {
 public static class NdiFinder
 {
     public static IEnumerable<string> sourceNames => EnumerateSourceNames();
-
+    public static IEnumerable<Interop.Source> sources => EnumerateSources();
+    
     public static IEnumerable<string> EnumerateSourceNames()
     {
         var list = new List<string>();
         foreach (var source in SharedInstance.Find.CurrentSources)
             list.Add(source.NdiName);
+        return list;
+    }
+    
+    public static IEnumerable<Interop.Source> EnumerateSources()
+    {
+        var list = new List<Interop.Source>();
+        foreach (var source in SharedInstance.Find.CurrentSources)
+            list.Add(source);
         return list;
     }
 }
