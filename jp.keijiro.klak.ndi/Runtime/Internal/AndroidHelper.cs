6f66e95a1443a5b778c8e711d215bbd766d90b36
Felix Herbst <felix.herbst@gmail.com>
2023-08-31 17:11:55 +0200

    Only do extra calls when targetSdkVersion >= 31

--- jp.keijiro.klak.ndi/Runtime/Internal/AndroidHelper.cs
+++ jp.keijiro.klak.ndi/Runtime/Internal/AndroidHelper.cs
@@ -22,12 +22,20 @@ static class AndroidHelper
         var activity = player.GetStatic<AndroidJavaObject>("currentActivity");
         _nsdManager = activity.Call<AndroidJavaObject>
           ("getSystemService", "servicediscovery");
-          
+
+        // Get applicationInfo and targetSdkVersion from activity.
+        var context = activity.Call<AndroidJavaObject>("getApplicationContext");
+        var info = context.Call<AndroidJavaObject>("getApplicationInfo");
+        var targetSdkVersion = info.Get<int>("targetSdkVersion");
+        
         // Start service discovery for NDI TCP and UDP services. Android API Level 31+ doesn't do that automatically anymore when calling `getSystemService`.
-        var tcpListener = new AndroidJavaObject("jp.keijiro.klak.ndi.DiscoveryListener");
-        var udpListener = new AndroidJavaObject("jp.keijiro.klak.ndi.DiscoveryListener");
-        _nsdManager.Call("discoverServices", "_ndi._tcp", 1, tcpListener);
-        _nsdManager.Call("discoverServices", "_ndi._udp", 1, udpListener);
+        if (targetSdkVersion >= 31)
+        {
+            var tcpListener = new AndroidJavaObject("jp.keijiro.klak.ndi.DiscoveryListener");
+            var udpListener = new AndroidJavaObject("jp.keijiro.klak.ndi.DiscoveryListener");
+            _nsdManager.Call("discoverServices", "_ndi._tcp", 1, tcpListener);
+            _nsdManager.Call("discoverServices", "_ndi._udp", 1, udpListener);
+        }
     }
 
     #else
